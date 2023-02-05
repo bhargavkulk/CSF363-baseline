0f006205ad6210f4cb8d11f97a395f1950f3d223
Nimish Wadekar <wadekar.nimish@gmail.com>
2023-02-06 03:09:59 +0530

    Add staged intermediate output generation

--- src/lexer.lex
+++ src/lexer.lex
@@ -25,3 +25,25 @@ extern int yyerror(std::string msg);
 .         { yyerror("unknown char"); }
 
 %%
+
+std::string token_to_string(int token, const char *lexeme) {
+    std::string s;
+    switch (token) {
+        case TPLUS: s = "TPLUS"; break;
+        case TDASH: s = "TDASH"; break;
+        case TSTAR: s = "TSTAR"; break;
+        case TSLASH: s = "TSLASH"; break;
+        case TSCOL: s = "TSCOL"; break;
+        case TLPAREN: s = "TLPAREN"; break;
+        case TRPAREN: s = "TRPAREN"; break;
+        case TEQUAL: s = "TEQUAL"; break;
+        
+        case TDBG: s = "TDBG"; break;
+        case TLET: s = "TLET"; break;
+        
+        case TINT_LIT: s = "TINT_LIT"; s.append("  ").append(lexeme); break;
+        case TIDENT: s = "TIDENT"; s.append("  ").append(lexeme); break;
+    }
+
+    return s;
+}
--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <cstdio>
 #include <cstring>
@@ -10,22 +11,49 @@
 
 extern FILE *yyin;
 extern int yylex();
-extern char* yytext;
+extern char *yytext;
 
-NodeStmts* final_values;
+NodeStmts *final_values;
 
-void lexer_output();
+#define ARG_OPTION_L 0
+#define ARG_OPTION_P 1
+#define ARG_OPTION_S 2
+#define ARG_OPTION_O 3
+#define ARG_FAIL -1
 
-int main(int argc, char* argv[]) {
+int parse_arguments(int argc, char *argv[]) {
+	if (argc == 3 || argc == 4) {
+		if (strlen(argv[2]) == 2 && argv[2][0] == '-') {
+			if (argc == 3) {
+				switch (argv[2][1]) {
+					case 'l':
+					return ARG_OPTION_L;
 
-	if(argc == 1) {
-		std::cerr << "Usage:\n";
-		std::cerr << "`base <file_name>`, to compile file and output to stdout\n";
-		std::cerr << "`base <file_name> <output>`, to compile file and output to <output>\n";
-        exit(1);
-    }
+					case 'p':
+					return ARG_OPTION_P;
 
-    bool output = argc == 3;
+					case 's':
+					return ARG_OPTION_S;
+				}
+			} else if (argv[2][1] == 'o') {
+				return ARG_OPTION_O;
+			}
+		}
+	} 
+	
+	std::cerr << "Usage:\nEach of the following options halts the compilation process at the corresponding stage and prints the intermediate output:\n\n";
+	std::cerr << "\t`./bin/base <file_name> -l`, to tokenize the input and print the token stream to stdout\n";
+	std::cerr << "\t`./bin/base <file_name> -p`, to parse the input and print the abstract syntax tree (AST) to stdout\n";
+	std::cerr << "\t`./bin/base <file_name> -s`, to compile the file to LLVM assembly and print it to stdout\n";
+	std::cerr << "\t`./bin/base <file_name> -o <output>`, to compile the file to LLVM bitcode and write to <output>\n";
+	return ARG_FAIL;
+}
+
+int main(int argc, char *argv[]) {
+	int arg_option = parse_arguments(argc, argv);
+	if (arg_option == ARG_FAIL) {
+		exit(1);
+	}
 
 	std::string file_name(argv[1]);
 	FILE *source = fopen(argv[1], "r");
@@ -37,23 +65,43 @@ int main(int argc, char* argv[]) {
 
 	yyin = source;
 
+	if (arg_option == ARG_OPTION_L) {
+		extern std::string token_to_string(int token, const char *lexeme);
+
+		while (true) {
+			int token = yylex();
+			if (token == 0) {
+				break;
+			}
+
+			std::cout << token_to_string(token, yytext) << "\n";
+		}
+		fclose(source);
+		return 0;
+	}
+
     final_values = nullptr;
 	yyparse();
 
+	fclose(source);
+
 	if(final_values) {
-		std::cout << final_values->to_string() << std::endl;
+		if (arg_option == ARG_OPTION_P) {
+			std::cout << final_values->to_string() << std::endl;
+			return 0;
+		}
+		
         llvm::LLVMContext context;
 		LLVMCompiler compiler(&context, "base");
 		compiler.compile(final_values);
-        if(output) {
-            compiler.write(std::string(argv[2]));
-        } else
-            compiler.dump();
-	}
-	else
+        if (arg_option == ARG_OPTION_S) {
+			compiler.dump();
+        } else {
+            compiler.write(std::string(argv[3]));
+		}
+	} else {
 	 	std::cerr << "empty program";
-
-	fclose(source);
+	}
 
     return 0;
 }
